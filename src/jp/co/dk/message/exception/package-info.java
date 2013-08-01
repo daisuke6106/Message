e2c00615a6f8212f87e2b76c88c5574682a92c32
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-08-01 16:10:21 +0000

    コミットしなおしのため破棄
    git-svn-id: file:///opt/svn/repository/projects/trunk@164 425babaa-d5dd-46a5-afc5-8c713db7da9f

--- src/jp/co/dk/message/exception/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
-  * メッセージを使用する例外クラスを保有するパッケージです。
-  * @since 1.0
-  * @author D.Kanno
-  */
-package jp.co.dk.message.exception;
--- src/jp/co/dk/message/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
-  * メッセージの管理を行うパッケージです。
-  * @since 1.0
-  * @author D.Kanno
-  */
-package jp.co.dk.message;
