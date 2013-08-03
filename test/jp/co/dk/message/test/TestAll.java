44000c4b38a9c9eb243182f3d38d414270554b07
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-08-03 11:45:43 +0000

    ライブラリ構成変更
    git-svn-id: file:///opt/svn/repository/projects/trunk@187 425babaa-d5dd-46a5-afc5-8c713db7da9f

---
+++ test/jp/co/dk/message/test/TestAll.java
@@ -0,0 +1,9 @@
+package jp.co.dk.message.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestCaseMessage.class })
+public class TestAll {}
